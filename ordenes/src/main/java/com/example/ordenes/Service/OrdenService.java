c05026f1d6aa2a8e12483b725d1d6a0dd35d394e
osvaldocozzi <meka__cozzi@hotmail.com>
2024-05-20 12:00:17 -0300

    commit

--- ordenes/src/main/java/com/example/ordenes/Service/OrdenService.java
+++ ordenes/src/main/java/com/example/ordenes/Service/OrdenService.java
@@ -1,12 +1,20 @@
 package com.example.ordenes.Service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.ordenes.Model.Orden;
 import com.example.ordenes.Repository.IOrdenRepository;
 
 @Service
 public class OrdenService {
     @Autowired
     IOrdenRepository ordenRepository;
+
+
+    public List<Orden> getOrdenes(){
+        return ordenRepository.findAll();
+    }
 }
---
+++ ordenes/src/main/java/com/example/ordenes/Service/UsuarioService.java
@@ -0,0 +1,5 @@
+package com.example.ordenes.Service;
+
+public class UsuarioService {
+    
+}
